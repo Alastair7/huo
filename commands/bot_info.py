f1478b5e05d4ba7feb60a82632fe5181589bda80
Alastair7 <alexandru.ban.drmer@gmail.com>
2021-07-26 13:16:19 +0200

    bot_info cog is completed.

--- commands/bot_info.py
+++ commands/bot_info.py
@@ -13,13 +13,16 @@ class Bot_info(commands.Cog):
         bot_user = ctx.bot.user
 
         botName = str(bot_user.name)
-        botBirthday = str(bot_user.created_at)
+        botDescription = f"Hello! my name is Hue. I'm a multipurpose bot with a mission: Make your life easier in your discord server." 
+        botBirthday = str(bot_user.created_at.strftime(date_format))
         botCreator = str(ctx.guild.owner)[:-5]
+        botServers = len(ctx.bot.guilds)
 
-        embed = discord.Embed(color=0x2E8B57)
+        embed = discord.Embed(description= botDescription, color=0x2E8B57)
         embed.set_author(name=botName, icon_url=bot_user.avatar_url)
         embed.add_field(name="Birthday", value=botBirthday, inline=True)
         embed.add_field(name='Creator', value=botCreator, inline=True)
+        embed.add_field(name=f"Living in", value=f"{botServers} servers", inline=True )
         # Get channel
         systemChannel = discord.utils.get(ctx.guild.channels, id=835107161608552470)
         await systemChannel.send(embed = embed)
