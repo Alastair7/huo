fffe5b446a7d9c2a3016bcee62983f27de22910c
Alastair7 <alexandru.ban.drmer@gmail.com>
2021-07-27 13:05:37 +0200

    Added kick cog

---
+++ commands/kick.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import discord
+
+class Kick(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command(name='kick')
+    async def kickUser(self, ctx, user:discord.Member,*,reason = None):
+
+        if reason is None:
+            await ctx.send(f"{user.mention} was kicked || Reason: None")
+            await user.kick()
+        else:
+            await ctx.send(f"{user.mention} was **kicked** || **Reason:** {reason}")
+            await user.kick(reason=reason)
+
+def setup(client):
+    client.add_cog(Kick(client))
