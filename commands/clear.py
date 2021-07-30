3a6e131003216eea7843619e556c38bbd9a013b4
Alastair7 <alexandru.ban.drmer@gmail.com>
2021-07-30 14:24:46 +0200

    Added clear cog error handler.

--- commands/clear.py
+++ commands/clear.py
@@ -5,8 +5,19 @@ class Clear(commands.Cog):
         self.client = client
     
     @commands.command(name='clear')
-    async def clear(self, ctx, amount=5):
+    async def clear(self, ctx, amount):
         await ctx.channel.purge(limit = amount + 1)
+    
+    @clear.error
+    async def clearError(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send("Bad arguments were introduced.")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Missing required arguments.")
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send("You don't have permissions to use this command.")
+        else:
+            await ctx.send("Something went wrong.")
 
 def setup(client):
     client.add_cog(Clear(client))
