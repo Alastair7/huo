41b06e132b659c6457aaf82d45abf74732b37534
Alastair7 <alexandru.ban.drmer@gmail.com>
2021-07-30 15:00:23 +0200

    Added mute/unmute error handling

--- commands/mute.py
+++ commands/mute.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from discord.ext.commands.errors import MemberNotFound
 
 class Mute(commands.Cog):
     def __init__(self, client):
@@ -20,6 +21,28 @@ class Mute(commands.Cog):
         muteRole = discord.utils.get(member.guild.roles, id=869894243022438400) 
         await member.remove_roles(muteRole)
         print(f'{member.name} unmuted')
+    
+    @muteUser.error
+    async def muteUserError(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You don't have the permissions to use this command.")
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send("Bad arguments were introduced.")
+        elif isinstance(error, commands.MemberNotFound):
+            await ctx.send("Member not found")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Missing required arguments.")
+    
+    @unMuteUser.error
+    async def unMuteUserError(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You don't have the permissions to use this command.")
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send("Bad arguments were introduced.")
+        elif isinstance(error, commands.MemberNotFound):
+            await ctx.send("Member not found")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Missing required arguments.")
 
 def setup(client):
     client.add_cog(Mute(client))
