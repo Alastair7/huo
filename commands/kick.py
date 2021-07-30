37bcc2755db3fba44b743f5d838873db13f2927a
Alastair7 <alexandru.ban.drmer@gmail.com>
2021-07-30 14:06:25 +0200

    Added error handler for kick cog.

--- commands/kick.py
+++ commands/kick.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from discord.ext.commands.errors import MissingPermissions
 
 class Kick(commands.Cog):
     def __init__(self, client):
@@ -15,5 +16,15 @@ class Kick(commands.Cog):
             await ctx.send(f"{member.mention} was **kicked** || **Reason:** {reason}")
             await member.kick(reason=reason)
 
+    @kickUser.error
+    async def kickUserError(self,ctx,error):
+        if isinstance(error,commands.BadArgument):
+            await ctx.send(f"Bad arguments were introduced.")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"Missing required arguments.")
+        elif isinstance(error, MissingPermissions):
+            await ctx.send(f"Missing permissions.")
+        else:
+            await ctx.send(f"Oops! something went wrong.")
 def setup(client):
     client.add_cog(Kick(client))
